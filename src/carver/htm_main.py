d916f52461d196395e32640637917159fcce8dcf
Jason Carver <bea8pyafww@snkmail.com>
2010-12-10 04:26:28 -0500

    use new HTM api for executing a time tick, and clean up htm_main

--- src/carver/htm_main.py
+++ src/carver/htm_main.py
@@ -3,34 +3,36 @@ Created on Nov 26, 2010
 
 @author: Jason Carver
 
-Numenta docs are (c) Numenta
 '''
-from carver.htm.config import config
 from carver.htm import HTM
-from numenta.htm import pool_spatial, pool_temporal
 
 if __name__ == '__main__':
     htm = HTM()
     
-    #TODO enable real data input
-    data = [[1,0,1],[0,0,1]] #2d format, same dimensions as htm (for now)
+    #prepare the initial data image in 2d format, same dimensions as htm, for now
+    data = [
+        [1,0,1,0,1,0,1,0,1,0],
+        [0,0,0,0,1,0,1,0,1,1],
+        [0,0,1,0,1,0,0,0,0,1],
+        [0,1,0,0,1,1,0,1,0,1],
+        [1,0,1,0,1,0,1,0,0,1],
+        [0,0,0,1,1,0,0,0,1,1],
+        ]
     
     htm.initialize_input(data)
     
-    #TODO run data over time
-    for t in xrange(1):
-        pool_spatial(htm)
-        pool_temporal(htm, learning=True)
-        
-        #TODO update data array
-        
-        for cell in htm.cells:
-            cell.clockTick()
+    #this method should update the data matrix in place
+    def dataUpdate(data):
+        'flip all bits in data matrix'
+        for x in xrange(len(data)):
+            data[x]=map(lambda bit: not bit, data[x])
     
-    #TODO show output
+    htm.execute(data, dataUpdate, ticks=30)
+    
+    #TODO show output more effectively
     for cell in htm.cells:
         print cell
     for col in htm.columns:
         print col
     
-    #TODO serialize htm state
+    #TODO save htm state to disk
