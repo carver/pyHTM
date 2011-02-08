56cec4f4191f8fff0e4cdcd2703c6510c3a8b520
Jason <Jason@192.168.1.103>
2011-02-08 12:17:50 -0500

    Add ColumnInfo class/tests for investigating state of spatial pooler

---
+++ src/carver/htm/ui/column_info.py
@@ -0,0 +1,38 @@
+'''
+Created on Feb 7, 2011
+
+@author: Jason
+'''
+
+class ColumnInfo(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, column):
+        '''
+        Constructor
+        '''
+        self.col = column
+        
+    def synapseStates(self):
+        states = []
+        for syn in self.col.synapses:
+            state = '?'
+            inputFiring = syn.is_firing(requireConnection=False)
+            if syn.connected:
+                if inputFiring:
+                    state = 'O'
+                else:
+                    state = '_'
+            else:
+                if inputFiring:
+                    state = 'X'
+                else:
+                    state = '.'
+            states.append(state)
+        return ' '.join(states)
+        
+    def synapseTargets(self):
+        return ' '.join(map(lambda syn: str(syn.input.location), self.col.synapses))
---
+++ src/carver/tests/htm/ui/column_info.py
@@ -0,0 +1,56 @@
+'''
+Created on Feb 7, 2011
+
+@author: Jason
+'''
+import unittest
+from carver.htm import HTM
+from carver.htm.io.flashcards import FlashCards
+from carver.htm.ui.column_info import ColumnInfo
+
+
+class ColumnInfoTest(unittest.TestCase):
+
+
+    def setUp(self):
+        mat1 = [[1,0,0],[0,1,1]]
+        mat2 = [[1,0,1],[0,1,0]]
+        mat3 = [[0,1,1],[1,0,0]]
+        self.matrices = [mat1, mat2, mat3]
+        cards = FlashCards(1, mat1, mat2, mat3)
+        htm = HTM()
+        htm.initialize_input(mat1)
+        htm.execute(mat1, dataModifyFunc=cards.updateMatrix, ticks=3*30, learning=True)
+        
+        self.htm = htm
+
+    def tearDown(self):
+        pass
+
+
+    def testSynapseReport(self):
+        htm = self.htm
+        first = htm.columns.next()
+        colInfo = ColumnInfo(first)
+        print colInfo.synapseTargets()
+        
+        htm.execute(self.matrices[0], learning=False)
+        states1 = colInfo.synapseStates()
+        
+        htm.execute(self.matrices[1], learning=False)
+        states2 = colInfo.synapseStates()
+        
+        htm.execute(self.matrices[2], learning=False)
+        states3 = colInfo.synapseStates()
+        
+        print states1
+        print states2
+        print states3
+        
+        self.assertNotEqual(states1, states2)
+        self.assertNotEqual(states2, states3)
+        self.assertNotEqual(states1, states3)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testSynapseReport']
+    unittest.main()
