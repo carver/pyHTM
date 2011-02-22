07864aae03fd70247a1fbd2badd50f360b99f32c
Jason <Jason@192.168.1.103>
2011-02-21 21:15:51 -0500

    add tests and implementation for new animator: translate data in x/y

---
+++ src/carver/htm/io/translate_input.py
@@ -0,0 +1,51 @@
+'''
+Created on Jan 31, 2011
+
+@author: Jason
+'''
+
+class TranslateInput(object):
+    '''
+    Take an input matrix and shift it over time.
+    '''    
+    
+    def __init__(self, init, shift, steps):
+        '''
+        @param init: a data matrix to start with
+        @param shift: a list with one element for each dimension of the init matrix,
+            representing the shift per time step
+        @param steps: Number of steps to execute
+        '''
+        self.current = init
+        self.steps_remaining = steps
+        self.shift = shift
+        
+    def dataGenerator(self):
+        while self.steps_remaining:
+            
+            self.current = self.doShift()
+            
+            yield self.current
+            
+            self.steps_remaining -= 1
+            
+    def doShift(self):
+        
+        #shift X
+        shifted = self.__shiftList(self.current, self.shift[0])
+        
+        #shift Y
+        if len(self.shift) > 1:
+            shifted = [ self.__shiftList(dim, self.shift[1]) for dim in shifted ]
+        
+        return shifted
+    
+    def __shiftList(self, original, byX):
+        if byX:
+            #shift along the X axis
+            shifted = original[byX*-1:]
+            shifted.extend(original[:byX*-1])
+            
+            return shifted
+        else:
+            return original
---
+++ src/carver/tests/htm/io/translate_input.py
@@ -0,0 +1,64 @@
+'''
+Created on Jan 31, 2011
+
+@author: Jason
+'''
+import unittest
+from carver.htm.io.translate_input import TranslateInput
+
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testShiftXUp(self):
+        a = [[1,2,3],[4,5,6],[7,8,9]]
+        ti = TranslateInput(a, shift=[1,0], steps=2)
+        
+        iterator = iter(ti.dataGenerator())
+        next = iterator.next()
+        self.assertEqual(next, [[7,8,9],[1,2,3],[4,5,6]])
+        next = iterator.next()
+        self.assertEqual(next, [[4,5,6],[7,8,9],[1,2,3]])
+        
+    def testShiftXDown(self):
+        a = [[1,2,3],[4,5,6],[7,8,9]]
+        ti = TranslateInput(a, shift=[-1,0], steps=2)
+        
+        iterator = iter(ti.dataGenerator())
+        next = iterator.next()
+        self.assertEqual(next, [[4,5,6],[7,8,9],[1,2,3]])
+        next = iterator.next()
+        self.assertEqual(next, [[7,8,9],[1,2,3],[4,5,6]])
+        
+    def testShiftYUp(self):
+        a = [[1,2,3],[4,5,6],[7,8,9]]
+        ti = TranslateInput(a, shift=[0,1], steps=2)
+        
+        iterator = iter(ti.dataGenerator())
+        next = iterator.next()
+        self.assertEqual(next, [[3,1,2],[6,4,5],[9,7,8]])
+        next = iterator.next()
+        self.assertEqual(next, [[2,3,1],[5,6,4],[8,9,7]])
+        
+    def testShiftYDown(self):
+        a = [[1,2,3],[4,5,6],[7,8,9]]
+        ti = TranslateInput(a, shift=[0,-1], steps=2)
+        
+        iterator = iter(ti.dataGenerator())
+        next = iterator.next()
+        self.assertEqual(next, [[2,3,1],[5,6,4],[8,9,7]])
+        next = iterator.next()
+        self.assertEqual(next, [[3,1,2],[6,4,5],[9,7,8]])
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testFlashCards']
+    unittest.main()
