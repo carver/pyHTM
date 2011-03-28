0fcff95d22127afec8b9ff98e2cd5293d19e66bf
Jason Carver <bea8pyafww@snkmail.com>
2011-03-28 12:14:16 -0400

    test temporal recognition, using imagination (incomplete)

---
+++ src/carver/tests/recognition_temporal.py
@@ -0,0 +1,141 @@
+'''
+Created on Dec 7, 2010
+
+@author: Jason Carver
+'''
+import unittest
+from carver.htm import HTM
+from carver.htm.ui.excite_history import ExciteHistory
+from carver.htm.io.objectrecognize import ObjectRecognize
+from carver.htm.io.translate_input import TranslateInput
+from PIL import Image
+from carver.htm.io.image_builder import ImageBuilder
+from carver.htm.segment import SEGMENT_ACTIVATION_THRESHOLD
+from carver.htm.synapse import SYNAPSES_PER_SEGMENT
+
+class TestRecognitionTemporal(unittest.TestCase):
+
+
+    def setUp(self):
+        self.left_block = [
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+            [1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+            [1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+            [1,1,1,1,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            ]
+        self.right_block = [
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+            [0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+            [0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+            [0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            ]
+        self.top_block = [
+            [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+            [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+            [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+            [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            ]
+        self.bottom_block = [
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+            [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+            [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+            [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+            ]
+
+    def tearDown(self):
+        pass
+
+    def testTemporalTest(self):
+        h = HTM(cellsPerColumn=3)
+        h.initialize_input(self.left_block)
+        
+        #show a whole left->right pass, 5 times
+        steps = 14*5*10
+        goright = TranslateInput(self.left_block, shift=(1,0))
+        h.execute(goright.dataGenerator(), ticks=steps-1)
+        
+        #show a whole right->left pass, 5 times
+        goleft = TranslateInput(self.right_block, shift=(-1,0))
+        h.execute(goleft.dataGenerator(), ticks=steps-1)
+        
+        #show a whole top->bottom pass, 5 times
+        godown = TranslateInput(self.top_block, shift=(0,1))
+        h.execute(godown.dataGenerator(), ticks=steps-1)
+        
+        #show a whole bottom->top pass, 5 times
+        goup = TranslateInput(self.bottom_block, shift=(0,-1))
+        h.execute(goup.dataGenerator(), ticks=steps-1)
+        
+        #TODO test imagination
+        #do three steps of block starting left and moving right
+        h.execute(dataGenerator=goright.dataGenerator(), ticks=3, 
+            learning=False)
+        
+        #imagine 9 more steps
+#        for _ in xrange(10):
+#            h.imagineNext()
+            
+        #downstream the last step to project back into the input,
+        #see if you have a block that is on the right side
+        h._imagineStimulate(h.columns)
+        allStimulation = [cell.stimulation for row in h._inputCells for cell in row] 
+        maxStim = max(allStimulation)
+        
+        white = (255,255,255)
+        black = (0,0,0)
+        percentSynapsesForActivation = float(SEGMENT_ACTIVATION_THRESHOLD)/SYNAPSES_PER_SEGMENT
+        def stimToRGB(stim):
+            percentStimulated = stim/maxStim
+            activated = percentStimulated >= percentSynapsesForActivation
+            if activated:
+                return (255,int(percentStimulated*255),255)
+            else:
+                return (0,int(percentStimulated*255),0)
+        
+        img = ImageBuilder([h.width, h.length], stimToRGB)
+        img.setData(allStimulation)
+        img.show()
+        
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testInit']
+    unittest.main()
